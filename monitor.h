7c97266b8122a55bb6f8081c31bef3f49192f317
Suman Kundu <kundu7878@gmail.com>
2025-08-05 10:40:33 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,30 +1,3 @@
 #pragma once
 
-struct PulseRateRange {
-    int minAge;
-    int maxAge;
-    float minPulse;
-    float maxPulse;
-};
-
-struct VitalChecks {
-    bool temperature;
-    bool pulseRate;
-    bool spo2;
-};
-
-// Returns 0 if any vital is out of range, 1 otherwise
-int vitalsOk(float temperature, float pulseRate, float spo2, int age);
-
-// Pure function to check each vital
-bool isTemperatureOk(float temperature);
-bool isPulseRateOk(float pulseRate, int age);
-bool isSpO2Ok(float spo2);
-
-// Alert function for I/O
-void printAlert(const char* message);
-VitalChecks checkAllVitals(float temperature, float pulseRate, float spo2,
-                           int age);
-const PulseRateRange* findPulseRangeForAge(int age);
-bool ageInRange(int age, const PulseRateRange& range);
-int handleVitalAlert(bool isOk, const char* message);
+int vitalsOk(float temperature, float pulseRate, float spo2);
