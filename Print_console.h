30a80989ab3a7bfe6ba8c05db66dab5e2bb4967e
Suman Kundu <kundu7878@gmail.com>
2025-08-19 09:21:32 +0530

    Delete Print_console.h

--- Print_console.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-// Function declarations for console printing and alerts
-void displayAlert(const std::string& message);
-void showVisualAlert();
-void handleCriticalVital(const std::string& vitalName);
